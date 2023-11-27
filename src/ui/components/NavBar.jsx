96943556d6c9fbb6ddfbae7bf1e9ce4337aac8ca
Bikcode <bikcodeh@gmail.com>
2023-11-27 15:29:02 -0500

    remove item from navbar

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -10,10 +10,6 @@ export const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-      <Link className="navbar-brand" to="/">
-        Asociaciones
-      </Link>
-
       <div className="navbar-collapse">
         <div className="navbar-nav">
           <NavLink className={ ({isActive}) => `nav-item nav-link ${ isActive ? 'active': ''}`} to="/marvel">
