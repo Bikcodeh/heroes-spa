4b041da81c64762caa04647906b0a31d658d68a7
Bikcode <bikcodeh@gmail.com>
2023-11-27 13:56:47 -0500

    use memo for hero card

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,9 +1,10 @@
+import { useMemo } from 'react';
 import { useParams, Navigate, useNavigate } from "react-router-dom";
 import { getHeroById } from "../helpers/getHeroById";
 
 export const HeroPage = () => {
   const { heroId } = useParams();
-  const hero = getHeroById(heroId);
+  const hero = useMemo( () => getHeroById(heroId), [heroId]);
   const navigate = useNavigate();
 
   const onNavigateBack = () => {
