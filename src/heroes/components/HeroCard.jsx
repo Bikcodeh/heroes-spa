94c8d026fefd9ac2cf142e77c7438500a277ce0c
Bikcode <bikcodeh@gmail.com>
2023-11-27 17:52:56 -0500

    apply responsive

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const CharactersByHero = ({alter_ego, characters}) => {
   if (characters.includes(alter_ego)) return (<p className="text-truncate">{characters}</p>);
@@ -13,13 +13,14 @@ export const HeroCard = ({
   first_appearance,
   characters,
 }) => {
+  const location = useLocation();
   const heroImageUrl = `assets/heroes/${id}.jpg`;
   return (
-    <div className="col animate__animated animate__fadeIn">
+    <div className={`${location.pathname === '/search' ? 'col' : 'col-xs-12 col-md-6 col-sm-12 col-lg-4'} animate__animated animate__fadeIn`}>
       <div className="card">
         <div className="row no-gutters">
           <div className="col-4">
-            <img src={heroImageUrl} className="card-img" alt={superhero} />
+            <img src={heroImageUrl} className="card-img" alt={superhero} style={{ height: '100%'}}/>
           </div>
           <div className="col-8">
             <div className="card-body">
--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -9,7 +9,7 @@ export const HeroList = ({ publisher }) => {
 
   return (
     <>
-    <div className="row rows-cols1 row-cols-md-3 g-3">
+    <div className="row rows-cols-sm-1 rows-cols-xs-1 row-cols-md-2 rows-cols-lg-3 g-3">
         {
             heroes.map(heroe => <HeroCard key={heroe.id} {...heroe} />) 
         }
