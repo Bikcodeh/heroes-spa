a78c23f58b574c82ceebab9f7e6de6574edc8f7b
Bikcode <bikcodeh@gmail.com>
2023-11-27 14:34:18 -0500

    add query string lib and implements search by query param in the same page with navigate

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,52 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+
+import { HeroCard } from "../components/HeroCard";
+import { useForm } from '../../hooks/useForm';
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse(location.search);
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.trim().length <= 1) return;
+    navigate(`?q=${searchText}`)
+  }
+
+  const { searchText, onInputChange } = useForm({ searchText: ''});
   return (
-    <div>SearchPage</div>
-  )
-}
+    <>
+      <h1>Search</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form onSubmit={onSearchSubmit}>
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
+            />
+            <button className="btn btn-outline-primary mt-1">Search</button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Result</h4>
+          <hr />
+          <div className="alert alert-primary">Search a hero</div>
+          <div className="alert alert-danger">Not hero found with {q}</div>
+          {/** <HeroCard /> */}
+        </div>
+      </div>
+    </>
+  );
+};
